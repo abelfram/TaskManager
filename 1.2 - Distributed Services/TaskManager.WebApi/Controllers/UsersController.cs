fa3c5b1b35f5938d5d375b0352e039ced50223a8
Abelfram <abelfram@hotmail.com>
2024-09-24 18:20:15 +0200

    Add project files.

---
+++ 1.2 - Distributed Services/TaskManager.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public void CreateUser()
+        {
+
+        }
+       
+        
+    }
+}
